2bf45fcde1795b9d57a74616c3f11c49b151efd7
Alexandra_08 <105152394+AL2002MI08@users.noreply.github.com>
2024-01-08 15:36:47 +0200

    Update audience auth

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -7,7 +7,7 @@ import { Auth0Provider } from "@auth0/auth0-react";
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Auth0Provider
-     domain="dev-omjwupug7w2kbo8z.us.auth0.com"
+     domain="dev-qwoff0hkndmodgcr.us.auth0.com"
      clientId="N2ZDUjwWsm4QlO63atGYbRSy88dfI7TC"
      authorizationParams={{
       redirect_uri: "https://kings-real-estate-agency.vercel.app/"
