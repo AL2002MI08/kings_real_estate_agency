7ef4fb4514576a21fad92543696a9d380fee3cb0
Alexandra_08 <105152394+AL2002MI08@users.noreply.github.com>
2024-01-10 21:46:47 +0200

    Update audience

--- server/config/auth0Config.js
+++ server/config/auth0Config.js
@@ -2,7 +2,7 @@ import {auth} from 'express-oauth2-jwt-bearer'
 
 
 const jwtCheck = auth({
-    audience: 'https://kings_real_estate_agency/',
+   audience: 'https://kings-real-estate-agency/',
     issuerBaseURL: 'https://dev-qwoff0hkndmodgcr.us.auth0.com/',
     tokenSigningAlg: 'RS256'
 })
